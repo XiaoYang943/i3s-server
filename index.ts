02b0c2200e1652806b391e6f6ab6e768dd1f4d46
heyiyang <heyiyang@linespace.cn>
2025-07-02 12:18:06 +0800

    fix: 修复打包后启动服务时，配置无法热更新的Bug

--- index.ts
+++ index.ts
@@ -1,12 +1,33 @@
 import cors from "@elysiajs/cors";
 import { Elysia } from "elysia";
-import { getConfig, loadConfig } from "./src/config/config-loader";
+import {getConfig, getConfigPath, loadConfig} from "./src/config/config-loader";
 import { loadArchiveRecord } from "./src/controllers/slpk-controller";
 import { SceneServerRoutes } from "./src/routes/scene-server.route";
+import chokidar from "chokidar";
+
+/**
+ * 热重载配置文件
+ */
+const setupHotReload =  () =>  {
+  const configPath = getConfigPath();
+  chokidar.watch(configPath).on("change", async () => {
+    console.log("---------");
+    console.log("✅ Config file changed. Reloading...");
+    try {
+      loadConfig();
+      await loadArchiveRecord();
+      console.log("✅ Scene records reloaded.");
+    } catch (err) {
+      console.error("❌ Error reloading config or scenes:", err);
+    }
+  });
+}
 
 async function startServer() {
 
   loadConfig();
+
+  setupHotReload();
 
   await loadArchiveRecord();
 
--- src/config/config-loader.ts
+++ src/config/config-loader.ts
@@ -1,19 +1,18 @@
 import fs from "node:fs";
 import YAML from "yaml";
-import chokidar from "chokidar";
-import {loadArchiveRecord} from "../controllers/slpk-controller";
+
 import {Config} from "../type";
 
 let appConfig: Config;
-
+let currentConfigPath = "../../config.yaml";
 // 加载配置文件
 export function loadConfig() {
     const configPathArg = Bun.argv.find(arg => arg.startsWith("--config_path="));
     const configPath = process.env.CONFIG_PATH
         || (configPathArg ? configPathArg.split("=")[1] : undefined)
         || Bun.argv[2]
-        || "./config.yaml";
-
+        || currentConfigPath;
+    currentConfigPath = configPath;
     console.log(`✅ Loading config from: ${configPath}`);
 
     const configFile = fs.readFileSync(configPath, "utf8");
@@ -26,15 +25,6 @@ export function getConfig() {
     return appConfig;
 }
 
-// 热重载
-chokidar.watch("./config.yaml").on("change", async () => {
-    console.log("---------");
-    console.log("✅ config.yaml changed. Reloading...");
-    try {
-        loadConfig();
-        await loadArchiveRecord();
-        console.log("✅ Scene records reloaded.");
-    } catch (err) {
-        console.error("❌ Error while reloading config or scenes:", err);
-    }
-});
+export function getConfigPath() {
+    return currentConfigPath;
+}
